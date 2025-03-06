b04a5f06acf387d7225400a841786a6196ff8e3f
Andrew Laskevych <an.laskevych@gmail.com>
2025-03-06 22:04:40 +0200

    Add Docker Compose

--- db/data.sql
+++ db/data.sql
@@ -2,20 +2,4 @@
 
 USE Calendula;
 
-/*
-"argon2": "^0.41.1",
-        "cors": "^2.8.5",
-        "ejs": "^3.1.10",
-        "express": "^4.21.0",
-        "express-validator": "^7.2.0",
-        "jsonwebtoken": "^9.0.2",
-        "multer": "^1.4.5-lts.1",
-        "mysql2": "^3.10.1",
-        "nodemailer": "^6.9.15",
-        "nodemon": "^3.1.7",
-        "swagger-ui-express": "^5.0.1",
-        "uuid": "^10.0.0"
-
-*/
-
 /* END - PROJECT DATA */
