e4c0cc1f8fc60356459c74c04718b757fd2ad091
Inessa Repeshko <inessa.repeshko92@gmail.com>
2025-03-12 23:49:21 +0200

    Add fake data generation scripts and user test data migration

---
+++ db/migrations/sqls/20250312212541-add-users-test-data-down.sql
@@ -0,0 +1,13 @@
+DELETE FROM Calendula.users
+WHERE email IN (
+                'jaime_zulauf@example.org',
+                'garry_brekke@example.com',
+                'jeanne_gleason@example.com',
+                'randal_white@example.com',
+                'tracey_waelchi@example.org',
+                'gabriel.farrell@example.com',
+                'jan.pfannerstill60@example.net',
+                'marty_bergstrom@example.org',
+                'veronica_willms27@example.com',
+                'jermaine_mueller32@example.org'
+    );
